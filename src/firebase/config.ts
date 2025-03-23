0371f0cb94e467aa3a4ee9028d525ddbba08ca4b
harsh-dexter <harshtekriwal52@gmail.com>
2025-03-23 20:19:42 +0530

    fixed-img-upload

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,7 +5,7 @@ export const firebaseConfig = {
   authDomain: "anon-chat-app-d7b33.firebaseapp.com",
   databaseURL: "https://anon-chat-app-d7b33-default-rtdb.firebaseio.com",
   projectId: "anon-chat-app-d7b33",
-  storageBucket: "anon-chat-app-d7b33.appspot.com",
+  storageBucket: "anon-chat-app-d7b33.firebasestorage.app",
   messagingSenderId: "186704437505",
   appId: "1:186704437505:web:885195aea184482838fb00"
 };
